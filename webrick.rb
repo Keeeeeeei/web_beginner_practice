db092b417b0c79e3345fa122e402f487a2507c75
Kei <1372012@g.iuhw.ac.jp>
2022-08-07 23:20:20 +0900

    update

--- webrick.rb
+++ webrick.rb
@@ -7,5 +7,33 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+ server.mount_proc("/form_get") do |req, res|
+ body = 
+ "<html><meta charset='UTF-8'>
+  <body>
+   クエリパラメータは#{req.query}です<br>
+   こんにちは#{req.query['username']}さん。
+   あなたの年齢は#{req.query['age']}ですね
+  </body>
+</html>"
+ res.status = 200
+ res['Content-Type'] = 'text/html'
+ res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+ body = 
+ "<html><meta charset='UTF-8'>
+  <body>
+   フォームデータは#{req.query}です<br>
+   こんにちは#{req.query['username']}さん。
+   あなたの年齢は#{req.query['age']}ですね
+  </body>
+</html>"
+ res.status = 200
+ res['Content-Type'] = 'text/html'
+ res.body = body
+end
+
 server.start
 
